b081e16f3f05c116da64ee00a7703848a0905c4c
pletine <pierre.letinevez@gmail.com>
2022-10-20 11:55:30 +0400

    Creation of 2 pages which will be part of the project of the personal web site

---
+++ threed.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="styles/style.css" />
+        <title>3D</title>
+    </head>
+    <body>
+        <article>
+            Objets 3D
+        </article>
+    </body>
+</html>
